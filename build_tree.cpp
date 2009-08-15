d5950b22f1ffa6a26f042bb5886cd0e4b727fcce
Joshua L. Phillips <jphillips7@ucmerced.edu>
2009-08-14 22:58:12 -0700

    More python...

--- build_tree.cpp
+++ build_tree.cpp
@@ -1,20 +1,23 @@
 #include <deque>
 #include <vector>
 #include <set>
-#include <multiset>
+#include <map>
 #include <string>
 #include <fstream>
 #include <sstream>
+#include <iostream>
+#include <algorithm>
 
 using namespace std;
 
 bool read_rules(string filename, 
 		vector<string>& symbols,
-		vector<vector<size_t> >& rules) {
-
+		vector<pair<size_t, vector<pair<size_t,size_t> > > >& rules) {
+  
   // Initial structures
   set<string> sorted_symbols;
-  vector<multiset<string> > raw_rules;
+  map<string,size_t> invert_symbols;
+  vector<pair<string, multiset<string> > > raw_rules;
 
   // Open a file
   ifstream infile;
@@ -31,31 +34,84 @@ bool read_rules(string filename,
     // Parse by spaces
     stringstream parser;
     parser << line;
-    
+
+    // Grab a rule
     string symbol;
-    symbol << parser;
+    parser >> symbol;
 
-    if (!parser.eof())
-      raw_rules.push_back(multiset<string>)
-
-    while (!parser.eof()) {
-      sorted_symbols.insert(symbol);
-      raw_rules[raw_rules.length()-1].insert(symbol);
+    if (!parser.eof()) {
+      symbols.push_back(symbol);
+      raw_rules.push_back(pair<string,multiset<string> >(symbol,multiset<string>()));
+      parser >> symbol;
     }
-			    
-    getline(infile,line)
+    
+    while (!parser.fail()) {
+      symbols.push_back(symbol);
+      raw_rules[raw_rules.size()-1].second.insert(symbol);
+      parser >> symbol;
+    }
+    
+    getline(infile,line);
   }
 
-  for (set<string>::iterator itr = sorted_symbols.begin();
-       itr != sorted_symbols.begin();
-       
-  }
+  sort(symbols.begin(),symbols.end());
+  vector<string>::iterator sym_itr = unique(symbols.begin(),symbols.end());
+  symbols.resize(sym_itr - symbols.begin());
 
-  for (size_t x = 0; x < raw_rules.length
+  // Put symbols into structures in order
+  for (int x = 0; x < symbols.size(); x++)
+    invert_symbols.insert(pair<string,size_t>(symbols[x],symbols.size()-1));
+
+  cout << "Symbols: " << symbols.size() << endl;
+
+  for (size_t x = 0; x < raw_rules.size(); x++) {
+    cout << "Rule " << x << ": " << raw_rules[x].first << " - ";
+
+    rules.push_back(pair<size_t,vector<pair<size_t,size_t> > >(invert_symbols.find(raw_rules[x].first)->second,vector<pair<size_t,size_t> >()));
+    
+    for (size_t y = 0; y < symbols.size(); y++) {
+      size_t count = raw_rules[x].second.count(symbols[y]);
+      cout << count << "*" << symbols[y] << " ";
+      if (count > 0)
+	rules[x].second.push_back(pair<size_t,size_t>(y,count));
+    }
+    cout << endl;
+  }
+  
+  if (symbols.size() == 0 || rules.size() == 0)
+    return false;
+  
   return true;
 }
 
-int main(int argc, 
+void print_rules(ostream& os,
+		 vector<string>& symbols,
+		 vector<pair<size_t, vector<pair<size_t,size_t> > > >& rules) {
+
+  for (size_t x = 0; x < rules.size(); x++) {
+    cout << symbols[rules[x].first] << " ";
+    for (size_t y = 0; y < rules[x].second.size(); y++) {
+      for (size_t w = 0; w < rules[x].second[y].second; w++)
+	cout << symbols[rules[x].second[y].first] << " ";
+    }
+    cout << endl;
+  }
+
+  return;
+}
+
+int main(int argc,
 	 char* argv[]) {
 
+  vector<string> symbols;
+  vector<pair<size_t, vector<pair<size_t,size_t> > > > rules;
+
+  cout << "Reading:" << endl;
+  if (argc > 1)
+    cout << read_rules(string(argv[1]),symbols,rules) << endl;
+
+  cout << "Writing:" << endl;
+  print_rules(cout,symbols,rules);
+  
+  return 0;
 }
--- build_tree.py
+++ build_tree.py
@@ -2,6 +2,27 @@
 
 import sys
 
-f = open(sys.argv[0],'r')
-while (read_data = f.readline()):
-    print read_data
+## Read rule file
+if len(sys.argv) > 1:
+    f = open(sys.argv[1],'r')
+    filedata = f.readlines()
+
+## Get symbols
+data = []
+for x in range(0,len(filedata)):
+    temp = filedata[x].split()
+    for y in range(0,len(temp)):
+        data.append(temp[y])
+## Sort symbols
+symbols = list(set(data))
+symbols_inv = dict()
+for x in range(0,len(symbols)):
+    symbols_inv[symbols[x]] = x
+
+## Get rules
+rules = list()
+for x in range(0,len(filedata)):
+    temp = filedata[x].split()
+    rules.append(list())
+    for y in temp: rules[x].append(symbols_inv[y])
+
